d16e99c759407208d32683314b34e26357e5222e
tgualchi <97109000+tgualchi@users.noreply.github.com>
2023-02-07 03:04:06 -0300

    cambio de carpeta

---
+++ src/app/app.module.ts
@@ -0,0 +1,74 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcercaDeComponent,
+    
+    BannerComponent,
+    EducacionComponent,
+    EditeducacionComponent,
+    NeweducacionComponent,
+    ExperienciaComponent,
+    EditExperienciaComponent,
+    NewExperienciaComponent,
+    FooterComponent,
+    HomeComponent,
+    HysComponent,
+    EditSkillComponent,
+    NewSkillComponent,
+    LoginComponent,
+    LogoAPComponent,
+    ProyectosComponent,
+    HeaderComponent,
+    EditAcercaDeComponent
+
+
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    
+
+  ],
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
