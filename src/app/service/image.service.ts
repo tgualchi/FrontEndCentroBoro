616d09980f073f630efa7bd1072bcd5fbd35a041
tgualchi <97109000+tgualchi@users.noreply.github.com>
2023-02-14 06:13:20 -0300

    cambios en el enviderment

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -6,28 +6,28 @@ import { Storage, ref, uploadBytes, list, getDownloadURL } from '@angular/fire/s
   providedIn: 'root'
 })
 export class ImageService {
-  url: string;
+  url: string = "";
 
   constructor(private storage: Storage) { }
 
-  public uploadImage($event: any, name: string){
-const file = $event.target.files[0]
-const imgRef = ref(this.storage, `imagen/`+ name )
-uploadBytes(imgRef, file)
-.then(response => {this.getImages()} )
-.catch(error => console.log(error)
-)
+  public uploadImage($event: any, name: string) {
+    const file = $event.target.files[0]
+    const imgRef = ref(this.storage, `imagen/` + name)
+    uploadBytes(imgRef, file)
+      .then(response => { this.getImages() })
+      .catch(error => console.log(error)
+      )
 
   }
-  getImages(){
-const imagesRef = ref(this.storage, 'imagen' )
-list(imagesRef)
-.then(async response => {
-  for(let item of response.items){
-    this.url = await getDownloadURL(item);
-    console.log("la URL es: " + this.url);
-  }
-})
-.catch(error => console.log(error))
+  getImages() {
+    const imagesRef = ref(this.storage, 'imagen')
+    list(imagesRef)
+      .then(async response => {
+        for (let item of response.items) {
+          this.url = await getDownloadURL(item);
+          console.log("la URL es: " + this.url);
+        }
+      })
+      .catch(error => console.log(error))
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,17 @@
 export const environment = {
   firebase: {
-    
+    projectId: 'frontendtg',
+    appId: '1:353169796496:web:c1433dba0031c17f7cd715',
+    storageBucket: 'frontendtg.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyD0EO8MnjEKN6o_sXBLktxoNW3diM_x4Yo',
+    authDomain: 'frontendtg.firebaseapp.com',
+    messagingSenderId: '353169796496',
+    measurementId: 'G-LEMGLF9EKY',
   },
     
     production: false,
    // URL: 'http://localhost:8080/',
-    URL: 'backend-production-ef5d.up.railway.app/'
+    URL: 'https://backendtob.onrender.com/'
 
   };
